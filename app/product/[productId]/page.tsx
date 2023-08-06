1636a89389fc53a47dfcd0e9483ab904153e4246
novaari2 <novaari20@gmail.com>
2023-08-06 23:31:56 +0700

    17 product quantity

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,3 +1,7 @@
+import Container from "@/app/components/Container";
+import ProductDetails from "./ProductDetails";
+import { product } from "@/utils/product";
+
 interface IParams{
     productId?: string
 }
@@ -5,8 +9,11 @@ interface IParams{
 const Product = ({params} : {params: IParams }) => {
     console.log("params ", params);
     
-    return ( <div>
-        Product Page
+    return ( 
+    <div className="p-8">
+        <Container>
+            <ProductDetails product={product}/>
+        </Container>
     </div> );
 }
  
